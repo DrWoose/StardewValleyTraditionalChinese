6874da12dfbadb8a049256a76df591b4a0e04d33
usa30311 <usa30311@gmail.com>
2024-03-29 14:49:20 +0800

    remove wrong file

--- Python Code/update_compare/tmp.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Change current working directory to where the script is located
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
-
-from glob import glob
-paths = glob('*/')
-print(paths)
